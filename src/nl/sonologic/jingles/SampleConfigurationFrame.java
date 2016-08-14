fb976ce092d4ed17d9e9aa8c05db5100691f24a6
Koen Martens <gmc@sonologic.nl>
2016-08-14 16:43:23 +0200

    Implement sample file dialog and sample loading.

--- src/nl/sonologic/jingles/SampleConfigurationFrame.java
+++ src/nl/sonologic/jingles/SampleConfigurationFrame.java
@@ -3,13 +3,13 @@
  */
 package nl.sonologic.jingles;
 
-import java.awt.Component;
-import java.awt.Container;
-import java.awt.Font;
 import java.awt.HeadlessException;
 import java.awt.event.ActionEvent;
 import java.io.File;
+import java.io.IOException;
 
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.AbstractAction;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
@@ -29,24 +29,13 @@ public class SampleConfigurationFrame extends JFrame {
 	 */
 	private static final long serialVersionUID = 6964754221771534749L;
 	private Context context;
+	private Sample sample = null;
+	private JTextField fileNameTextField;
+	private JLabel statusLabel;
 
 	public SampleConfigurationFrame(Context context) throws HeadlessException {
 		super("Sample configuration");
 		this.context = context;
-	}
-
-	private void setFileChooserFont(Component[] comp, Font font)
-	{  
-	  for(int x = 0; x < comp.length; x++)  
-	  {  
-	    if(comp[x] instanceof Container) setFileChooserFont(((Container)comp[x]).getComponents(), font);  
-	    try{comp[x].setFont(font);}  
-	    catch(Exception e){}//do nothing  
-	  }  
-	}  
-	
-	public void configureSample(Deck deck, int i) {
-		setTitle("Configure " + deck.getLabel() +", sample "+Integer.toString(i));
 		
 		SpringLayout layout = new SpringLayout();
 		
@@ -55,39 +44,60 @@ public class SampleConfigurationFrame extends JFrame {
 		JLabel label = new JLabel("File");
 		label.setFont(context.configFont);
 				
-		JTextField textField = new JTextField("", 35);
-		textField.setFont(context.configFont);
+		fileNameTextField = new JTextField("", 35);
+		fileNameTextField.setFont(context.configFont);
 		
 		JButton button = new JButton("browse");
 		button.setFont(context.configFont);
 		
 		class BrowseButtonAction extends AbstractAction {
 			private static final long serialVersionUID = 2165346479772650227L;
-
-			private JTextField textField;
-
 			private Context context;
+			private SampleConfigurationFrame frame;
 			
-			public BrowseButtonAction(Context context, JTextField textField, String label)
+			public BrowseButtonAction(Context context, SampleConfigurationFrame frame, String label)
 			{
 				super(label);
 				this.context = context;
-				this.textField = textField;
+				this.frame = frame;
 			}
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				JFileChooser fc = new JFileChooser();
 				//setFileChooserFont(fc.getComponents(), context.configFont);
-				int result = fc.showOpenDialog(textField);
+				int result = fc.showOpenDialog(frame.fileNameTextField);
 				
 				if(result==JFileChooser.APPROVE_OPTION) {
 					File file = fc.getSelectedFile();
-					textField.setText(file.getAbsolutePath());
+					frame.fileNameTextField.setText(file.getAbsolutePath());
+					statusLabel.setText("Loading sample...");
+					
+					boolean failed = false;
+					try {
+						frame.getSample().setFilename(frame.getFilename());
+					} catch (UnsupportedAudioFileException e1) {
+						statusLabel.setText("Error: Unsupported audio format");
+						failed = true;
+					} catch (IOException e1) {
+						statusLabel.setText("Error: IO error (unable to read file)");
+						failed = true;
+					} catch (LineUnavailableException e1) {
+						statusLabel.setText("Error: line unavailable");
+						failed = true;
+					} catch (InterruptedException e1) {
+						statusLabel.setText("Error: interrupted, try again");
+						failed = true;
+					}
+					if(!failed)
+						statusLabel.setText("Sample loaded");
 				}
 			}
 		}
 		
-		button.setAction(new BrowseButtonAction(context, textField, "browse"));
+		button.setAction(new BrowseButtonAction(context, this, "browse"));
+
+		statusLabel = new JLabel();
+		statusLabel.setFont(context.configFont);
 		
 		layout.putConstraint(SpringLayout.WEST, label,
                 5,
@@ -96,26 +106,48 @@ public class SampleConfigurationFrame extends JFrame {
                 5,
                 SpringLayout.NORTH, this);
 		
-		layout.putConstraint(SpringLayout.WEST, textField,
+		layout.putConstraint(SpringLayout.WEST, fileNameTextField,
                 5,
                 SpringLayout.EAST, label);
-		layout.putConstraint(SpringLayout.NORTH, textField,
+		layout.putConstraint(SpringLayout.NORTH, fileNameTextField,
                 5,
                 SpringLayout.NORTH, this);
 
 		layout.putConstraint(SpringLayout.WEST, button,
                 5,
-                SpringLayout.EAST, textField);
+                SpringLayout.EAST, fileNameTextField);
 		layout.putConstraint(SpringLayout.NORTH, button,
                 5,
                 SpringLayout.NORTH, this);
 
+		layout.putConstraint(SpringLayout.NORTH, statusLabel,
+				5,
+				SpringLayout.SOUTH, label);
+		
 		add(label);
-		add(textField);
+		add(fileNameTextField);
 		add(button);
+		add(statusLabel);
 		
 		pack();
+	}
+	
+	public void configureSample(Deck deck, int i) {
+		setTitle("Configure " + deck.getLabel() + ", sample "+Integer.toString(i));
+		statusLabel.setText("");
+		
+		sample = deck.getSample(i);
+		
+		this.fileNameTextField.setText(sample.getFilename());
 		
 		setVisible(true);
+	}
+
+	public Sample getSample() {
+		return sample;
+	}
+	
+	public String getFilename() {
+		return fileNameTextField.getText();
 	}
 }
